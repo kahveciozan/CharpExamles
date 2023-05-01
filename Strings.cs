5f9c2f1939b5ffe4450674e2d0d17640bdbdbee7
ozan kahveci <ozan.kahveci15@gmail.com>
2023-05-01 16:05:34 +0300

    Parsing string and casting

--- Strings.cs
+++ Strings.cs
@@ -1,4 +1,7 @@
-﻿namespace CSharpExamples
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Globalization;
+
+namespace CSharpExamples
 {
     public class Strings
     {
@@ -31,7 +34,47 @@
 
             int lengthOfPlanetWord = planet.Length; ;
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            // Parsing And Casting Features
+            Console.WriteLine("\n--- Parsing And Casting Features ---");
+            // Parsing
+            string ageStr = "36";
+            string addedAgeStr = "10";
+
+            int totalAge = int.Parse(ageStr) + int.Parse(addedAgeStr);
+
+            string weightStr = "203,34";                                     // !!! I did not use '.' because of CALTURE issue
+            string addedWeightStr = "12,26";                                 // If your PC has different CALURE. Use '.' instead od ','
+
+            double totalWeight = double.Parse(weightStr) + double.Parse(addedWeightStr);
+
+            Console.WriteLine($"New Age: {totalAge}");
+            Console.WriteLine($"New Weight: {totalWeight}");
+
+            // How to fix issues with inputs
+            string weightStr2 = "200,345";
+            bool didThisWork =  double.TryParse(weightStr2, out double newWeight);
+
+            string moneyEarnedStr = "1000,53";
+            bool didThisWorkADecimal = decimal.TryParse(moneyEarnedStr,
+                NumberStyles.AllowCurrencySymbol | NumberStyles.AllowDecimalPoint,
+                NumberFormatInfo.CurrentInfo, 
+                out decimal moneyEarnedAsDecimal);
+
+            Console.WriteLine($"Money Earned As Decimal: {moneyEarnedAsDecimal}");
+
+            decimal roundUp = Math.Round(moneyEarnedAsDecimal, 0);
+            //decimal roundMid = Math.Round(moneyEarnedAsDecimal, 0, MidpointRounding.ToZero);
+            Console.WriteLine($"Rounded Up Money: {roundUp}");
+
+
+            // Casting 
+            int moneyEarnedAsInt = (int)moneyEarnedAsDecimal;
+            float moneyEarnedAsFloat = (float)moneyEarnedAsDecimal;
+
+
+            //Console.ReadLine();
         }
 
 
